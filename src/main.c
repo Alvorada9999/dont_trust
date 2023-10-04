c2539dcd2c36185860179d7ba9ea96bc7a755412
Alvorada9999 <kenedyhenrique.buenosilva@gmail.com>
2023-10-04 12:59:38 -0300

    error handling addition

--- src/main.c
+++ src/main.c
@@ -4,12 +4,14 @@
 #include <stdbool.h>
 
 #include "init.h"
+#include "dt_signal_util.h"
 
 int main(int argc, char *argv[]) {
   Configs configs;
   memset(&configs, 0, sizeof(Configs));
   configs.shouldActAsServer = true;
-  blockAllSignals();
+  if(blockAllSignals() != 0)
+    return -1;
   getConfigs(argc, argv, &configs);
 
   return 0;
